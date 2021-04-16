511d1b83e7011065b5e9006423ef1133d8d210bb
Benito <bbxblack@gmail.com>
2021-04-16 11:20:15 +0200

    esercizio live

--- js/script.js
+++ js/script.js
@@ -5,18 +5,53 @@
 
 
 function boxNumberGenerator() {
-  $.ajax({
-      url: 'https://flynn.boolean.careers/exercises/api/random/int',
-      method: 'GET',
-      success: function(data) {
-          const res = data.response;
-          console.log(res);
-          stampBoxes(res);
-      },
-      error: function() {
-          console.log('error');
-      }
-  });
+  // $.ajax({
+  //     url: 'https://flynn.boolean.careers/exercises/api/random/int',
+  //     method: 'GET',
+  //     success: function(data) {
+  //         const res = data.response;
+  //         console.log(res);
+  //         stampBoxes(res);
+  //     },
+  //     error: function() {
+  //         console.log('error');
+  //     }
+  // });
+
+
+  // AXIOS
+  new Vue({
+        el: '#app',
+        data: {
+            values: []
+        },
+        mounted() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/integers',
+                {
+                    params: {
+                        min: 1,
+                        max: 100,
+                        items: 10
+                    }
+                })
+                .then(data => {
+                    // STRATEGIA 1
+                    // this.values = data.data.response;
+                    // STRATEGIA 2
+                    const values = data.data.response;
+                    for (let i=0;i<values.length;i++) {
+                        const value = values[i];
+                        const isPair = value % 2 == 0;
+                        this.values.push({
+                            value,
+                            class: isPair ? 'bg-green' : 'bg-red'
+                        });
+                    }
+                })
+                .catch(() => console.log('error'));
+        }
+    });
+}
   }
 
   function stampBoxes(num) {
