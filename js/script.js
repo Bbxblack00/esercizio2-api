9baa24db0964a86539cc28c72084b3832e0b689f
Benny <bbxblack@gmail.com>
2021-04-16 10:23:25 +0200

    esercizio live

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
-function mailGenerator() {
-    $.ajax({
-        url: '',
-        method: 'GET',
-        success: function(data) {
-            const res = data.response;
-            console.log(res);
-            generateMail(res);
-        },
-        error: function() {
-            console.log('error');
-        }
-    });
+// attraverso vue&axios scaricare 10 numeri interi e produrre le box
+// corrispondenti, colorandone il bg verde nel caso di numeri pari
+// e rossi nel caso di numeri dispari
+
+
+
+function boxNumberGenerator() {
+  $.ajax({
+      url: 'https://flynn.boolean.careers/exercises/api/random/int',
+      method: 'GET',
+      success: function(data) {
+          const res = data.response;
+          console.log(res);
+          stampBoxes(res);
+      },
+      error: function() {
+          console.log('error');
+      }
+  });
+  }
+
+  function stampBoxes(num) {
+
+
   }
 
 function init() {
+
+  boxNumberGenerator();
+
 }
 document.addEventListener('DOMContentLoaded', init);
